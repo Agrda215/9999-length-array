cb55dba21d2f85715d065923bb15806a553dc2d1
Agrda215 <107811732+Agrda215@users.noreply.github.com>
2022-08-02 09:44:37 -0300

    Initial commit

---
+++ Sprite2/Sprite2.js
@@ -0,0 +1,36 @@
+/* eslint-disable require-yield, eqeqeq */
+
+import {
+  Sprite,
+  Trigger,
+  Watcher,
+  Costume,
+  Color,
+  Sound
+} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+
+export default class Sprite2 extends Sprite {
+  constructor(...args) {
+    super(...args);
+
+    this.costumes = [
+      new Costume("costume1", "./Sprite2/costumes/costume1.svg", {
+        x: 172.25,
+        y: 41.916677474975586
+      })
+    ];
+
+    this.sounds = [new Sound("pop", "./Sprite2/sounds/pop.wav")];
+
+    this.triggers = [
+      new Trigger(Trigger.GREEN_FLAG, this.whenGreenFlagClicked)
+    ];
+  }
+
+  *whenGreenFlagClicked() {
+    for (let i = 0; i < 9999; i++) {
+      0;
+      yield;
+    }
+  }
+}
